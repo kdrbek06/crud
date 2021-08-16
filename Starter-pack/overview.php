5fb9c46160281df9213fad455b68b72f83ac2494
kdrbek06 <kdrbek@gmail.com>
2021-08-16 16:55:43 +0200

    Add form to overview

--- Starter-pack/overview.php
+++ Starter-pack/overview.php
@@ -13,9 +13,26 @@
 
 <ul>
     <?php foreach ($cards as $card) : ?>
-        <li><?= $card['Name'] . $card['HP']  .$card['Skills'] . $card['PokemonLevel'] ?></li>
+        <li><?= 'Pokemon Name; ' . $card['Name'] . '  HP; ' . $card['HP'] . '  Skills; '. $card['Skills']?></li>
     <?php endforeach; ?>
 </ul>
+
+// TODO: Create a form
+
+<form action="" method="post">
+
+    <label for="Name">Pokemon Name:</label>
+    <input type="text" name="Name" id="name">
+
+
+    <label for="Skills">Skills: </label>
+    <inpul type="text" name="Name"></inpul>
+
+
+</form>
+
+
+
 
 </body>
 </html>
