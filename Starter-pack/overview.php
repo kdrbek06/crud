40cfb28a12218a52cc1bf12511897ff4cfd2ad2b
kdrbek06 <kdrbek@gmail.com>
2021-08-17 14:29:35 +0200

    Add edit button and some style to pokemon list table

--- Starter-pack/overview.php
+++ Starter-pack/overview.php
@@ -6,16 +6,50 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Goodcard - track your collection of Pokémon cards</title>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+    </style>
 </head>
 <body>
 
 <h1>Goodcard - track your collection of Pokémon cards</h1>
 
-<ul>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>HP</th>
+        <th>Skill</th>
+        <th>Edit Card</th>
+
+    </tr>
     <?php foreach ($cards as $card) : ?>
-        <li><?= 'Pokemon Name; ' . $card['name'] . '  HP; ' . $card['hp'] . '  Skill; '. $card['skill']?></li>
+    <tr>
+        <td><?= $card['id'] ?> </td>
+        <td><?= $card['name'] ?> </td>
+        <td><?= $card['hp'] ?> </td>
+        <td><?= $card['skill'] ?> </td>
+        <td><a href="">Edit</a></td>
+
+
+    </tr>
+
     <?php endforeach; ?>
-</ul>
+</table>
 
 // TODO: Create a form
 
